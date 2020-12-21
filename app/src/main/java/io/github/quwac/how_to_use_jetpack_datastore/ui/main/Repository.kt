43be9db02af06e48fa32073151ad940c0b033d5a
quwac <53551867+quwac@users.noreply.github.com>
2020-12-21 23:37:27 +0900

    Add Wire and modify code for it

--- app/src/main/java/io/github/quwac/how_to_use_jetpack_datastore/ui/main/Repository.kt
+++ app/src/main/java/io/github/quwac/how_to_use_jetpack_datastore/ui/main/Repository.kt
@@ -1,6 +1,7 @@
 package io.github.quwac.how_to_use_jetpack_datastore.ui.main
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.Serializer
@@ -11,8 +12,13 @@ import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
 import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
 import com.example.application.Settings
+import com.squareup.moshi.Moshi
+import com.squareup.wire.WireJsonAdapterFactory
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import okio.buffer
+import okio.sink
+import okio.source
 import java.io.InputStream
 import java.io.OutputStream
 
@@ -37,39 +43,56 @@ class Repository(context: Context) {
         }
     }
 
-    object SettingsSerializer : Serializer<Settings> {
-        override fun readFrom(input: InputStream): Settings {
-            try {
-                return Settings.parseFrom(input)
-            } catch (exception: InvalidProtocolBufferException) {
-                throw CorruptionException("Cannot read proto.", exception)
+object SettingsSerializer : Serializer<Settings> {
+    private val moshi = Moshi.Builder()
+        .add(WireJsonAdapterFactory())
+        .build()
+    private val adapter = moshi.adapter(Settings::class.java)
+
+    override val defaultValue: Settings
+        get() = Settings()
+
+    override fun readFrom(input: InputStream): Settings {
+        return try {
+            input.source().buffer().use { source ->
+                adapter.fromJson(source)!!
             }
+        } catch (t: Throwable) {
+            // failure
+            Log.w("readFrom", "readFrom failed.", t)
+            Settings()
         }
-
-        override fun writeTo(
-            t: Settings,
-            output: OutputStream
-        ) = t.writeTo(output)
-
-        override val defaultValue: Settings
-            get() = Settings.getDefaultInstance()
     }
 
+    override fun writeTo(
+        t: Settings,
+        output: OutputStream
+    ) {
+        Log.e("writeTo", "writeTo invoked.")
+        try {
+            output.sink().buffer().use { sink ->
+                adapter.toJson(sink, t)
+            }
+        } catch (t: Throwable) {
+            // failure
+            Log.e("writeTo", "writeTo failed.", t)
+        }
+    }
+}
+
     private val protoDataStore: DataStore<Settings> = context.createDataStore(
-        fileName = "settings.pb",
+        fileName = "settings.pb.json",
         serializer = SettingsSerializer
     )
 
     fun getExampleNumberFlow_proto(): Flow<Int> = protoDataStore.data
         .map { settings ->
-            settings.exampleCounter
+            settings.example_counter
         }
 
     suspend fun save_proto(newValue: Int) {
         protoDataStore.updateData { currentSettings ->
-            currentSettings.toBuilder()
-                .setExampleCounter(newValue)
-                .build()
+            currentSettings.copy(example_counter = newValue)
         }
     }
 }
